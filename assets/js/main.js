d995ef42778c39f745dd650bb2db5bc6cd9d733e
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-02-12 12:05:17 +0100

    wrote showHeroes function

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,3 +21,34 @@ function populateHeader(jsonObj) {
     "Hometown: " + jsonObj["homeTown"] + " // Formed: " + jsonObj["formed"];
   header.appendChild(myPara);
 }
+
+function showHeroes(jsonObj) {
+  let heroes = jsonObj["members"];
+  for (let i = 0; i < heroes.length; i++) {
+    let myArticle = document.createElement("article");
+    let myH2 = document.createElement("h2");
+    let myPara1 = document.createElement("p");
+    let myPara2 = document.createElement("p");
+    let myPara3 = document.createElement("p");
+    let myList = document.createElement("ul");
+
+    myH2.textContent = heroes[i].name;
+    myPara1.textContent = "Secret Identity: " + heroes[i].secretIdentity;
+    myPara2.textContent = "Age: " + heroes[i].age;
+    myPara3.textContent = "Superpowers: ";
+
+    let superPowers = heroes[i].powers;
+    for (let j = 0; j < superPowers.length; j++) {
+      let listItem = document.createElement("li");
+      listItem.textContent = superPowers[j];
+      myList.appendChild(listItem);
+    }
+    myArticle.appendChild(myH2);
+    myArticle.appendChild(myPara1);
+    myArticle.appendChild(myPara2);
+    myArticle.appendChild(myPara3);
+    myArticle.appendChild(myList);
+
+    section.appendChild(myArticle);
+  }
+}
