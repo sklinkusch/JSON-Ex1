65ed52b62cead5afce008f7640a5d9e96d1d1d84
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-02-12 11:49:06 +0100

    built onload function

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,3 +4,10 @@ let requestURL =
   "https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json";
 let request = new XMLHttpRequest();
 request.open("GET", requestURL);
+request.responseType = "json";
+request.send();
+request.onload = function() {
+  let superHeroes = request.response;
+  populateHeader(superHeroes);
+  showHeroes(superHeroes);
+};
